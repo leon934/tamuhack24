a5dc5c95053b241381e8e5177928d2e35d9e2d94
Toren Long <venlercode@gmail.com>
2024-01-27 23:20:12 -0600

    Add logo

--- frontend/tamuhack/src/navbar.js
+++ frontend/tamuhack/src/navbar.js
@@ -1,10 +1,13 @@
 function Navbar() {
     return (
-        <ul id="navbar-content">
-            <li><a href={"/"}>Home</a></li>
-            <li><a href={"/openrunways"}>Open Runways</a> </li>
-            <li><a href={"/AI"}>AI Software</a> </li>
-        </ul>
+        <div id="navbar">
+            <ul id="navbar-content">
+                <li><a href={"/"}>Home</a></li>
+                <li><a href={"/openrunways"}>Open Runways</a> </li>
+                <li><a href={"/AI"}>AI Software</a> </li>
+            </ul>
+            <p id="logo"><span style={{ color: "blue" }}>AI</span><span style={{ color: "grey" }}>r</span><span style={{ color: "red" }}>Control</span></p>
+        </div>
     );
 }
 export default Navbar;
